d594ac3a809e09dedbd06dbce2e04524ace3e806
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2020-01-14 11:31:57 +0200

    Implement better tagging (#1)
    - Generate minor tag
    - Support `TAG_PREFIX` env variable to add prefix to resulting tags

--- script.sh
+++ script.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 if test "$DRONE_BUILD_EVENT" = 'push';then
   if test "$DRONE_BRANCH" = 'master';then
-    echo 'latest' > .tags
+    echo "${PLUGIN_TAG_PREFIX}latest" > .tags
   else
-    echo "$DRONE_BRANCH" > .tags
+    echo "${PLUGIN_TAG_PREFIX}${DRONE_BRANCH}" > .tags
   fi
 elif test "$DRONE_BUILD_EVENT" = 'tag';then
   TAG="${DRONE_TAG##v}"
-  MAJOR_TAG="${TAG%%.*}"
+  MINOR_TAG="${PLUGIN_TAG_PREFIX}${TAG%.*}"
+  MAJOR_TAG="${PLUGIN_TAG_PREFIX}${TAG%%.*}"  
 
-  echo "$TAG,$MAJOR_TAG" > .tags
+  echo "${PLUGIN_TAG_PREFIX}${TAG},${MINOR_TAG},${MAJOR_TAG}" > .tags
 fi
