168dbf5dcabd6fdd4f518d9c28769b6cec479595
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2020-01-14 11:29:02 +0200

    test

--- script.sh
+++ script.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 if test "$DRONE_BUILD_EVENT" = 'push';then
   if test "$DRONE_BRANCH" = 'master';then
-    echo "${TAG_PREFIX}latest" > .tags
+    echo "${PLUGIN_TAG_PREFIX}latest" > .tags
   else
-    echo "${TAG_PREFIX}${DRONE_BRANCH}" > .tags
+    echo "${PLUGIN_TAG_PREFIX}${DRONE_BRANCH}" > .tags
   fi
 elif test "$DRONE_BUILD_EVENT" = 'tag';then
   TAG="${DRONE_TAG##v}"
-  MINOR_TAG="${TAG_PREFIX}${TAG%.*}"
-  MAJOR_TAG="${TAG_PREFIX}${TAG%%.*}"  
+  MINOR_TAG="${PLUGIN_TAG_PREFIX}${TAG%.*}"
+  MAJOR_TAG="${PLUGIN_TAG_PREFIX}${TAG%%.*}"  
 
-  echo "${TAG_PREFIX}${TAG},${MINOR_TAG},${MAJOR_TAG}" > .tags
+  echo "${PLUGIN_TAG_PREFIX}${TAG},${MINOR_TAG},${MAJOR_TAG}" > .tags
 fi
